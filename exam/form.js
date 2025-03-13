496cb2651eebf90d5fdc56af9f2d9316d3356a9d
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-03-13 09:31:29 +0600

    java exam

---
+++ exam/form.js
@@ -0,0 +1,55 @@
+function datasend() {
+
+
+    let user1 = document.getElementById("name").value;
+    let contact = document.getElementById("contact").value;
+    let remarks = document.getElementById("remarks").value;
+    let gender = document.querySelector('input[name="gender"]:checked');
+    let course = document.querySelectorAll('input[name="course"]:checked');
+    let location = document.getElementById("location").value;
+
+    let courseValue=[];
+    for( let i=0; i < course.length; i++){
+        courseValue.push(course[i].value);
+    
+    }
+
+
+let output="name:" + user1 +"\n" +
+            "contact:" + contact +"\n" +
+            "remarks:" + remarks +"\n" +
+            "gender:" + gender.value +"\n" +
+            "course:" +  courseValue+"\n" +
+            "location:" + location +"\n" ;
+
+
+            let newWindow=window.open("","_blank");
+            newWindow.document.write("<pre>"+output+"</pre>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
